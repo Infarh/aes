84cddb5927121f5482fc1f8334a7ba468418184f
Infarh <shmachilin@yandex.ru>
2020-08-01 14:19:03 +0300

    Команды

---
+++ FileEncryptor.WPF/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private bool _Executable;
+
+        public bool Executable
+        {
+            get => _Executable;
+            set
+            {
+                if (_Executable == value) return;
+                _Executable = value;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter) => _Executable && CanExecute(parameter);
+
+        void ICommand.Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                Execute(parameter);
+        }
+
+        protected virtual bool CanExecute(object parameter) => true;
+
+        protected abstract void Execute(object parameter);
+    }
+}
---
+++ FileEncryptor.WPF/Infrastructure/Commands/LAmbdaCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FileEncryptor.WPF.Infrastructure.Commands.Base;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands
+{
+    class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action Execute, Func<bool> CanExecute = null)
+            : this(
+                Execute: p => Execute(), 
+                CanExecute: CanExecute is null ? (Func<object, bool>)null : p => CanExecute())
+        {
+            
+        }
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        protected override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        protected override void Execute(object parameter) => _Execute(parameter);
+    }
+}
