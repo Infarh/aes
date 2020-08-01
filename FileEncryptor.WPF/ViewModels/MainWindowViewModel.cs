bbec5341bc2190821aa7792a58abfad5234c619d
Infarh <shmachilin@yandex.ru>
2020-08-01 14:10:16 +0300

    Модель-представления

---
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FileEncryptor.WPF.ViewModels.Base;
+
+namespace FileEncryptor.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Шифратор";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
